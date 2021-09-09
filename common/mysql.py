2d21071fe2024fac08788284cb067a226d60bc66
MogulKhan <smileflyking@outlook.com>
2021-09-09 20:56:07 +0800

    0909:Khan获取科创版每日行情数据

--- common/mysql.py
+++ common/mysql.py
@@ -3,7 +3,7 @@ import pymysql
 def connectDB():
     try:
         conn = pymysql.connect(host='127.0.0.1', user='root', password='123456', database='wpf', charset='utf8')
-    except ee :
+    except Exception as ee :
         print('数据库链接失败！', str(ee))
     else:#try没有异常的时候才会执行
         print("数据库连接sucessfully!")
